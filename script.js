5d8d84a8ccfe8017cbdae9309ad14e3f0159492b
Shlaf <ivanlundberg93@gmail.com>
2017-08-22 13:03:45 +0200

    js fyllning

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Brings the form from the toDo list into focus and calls submit event's function */
+$('form').submit(function () {
+    /* If the input value is not an emtpy string */
+    if ($('input').val() !== '') {
+        /* Creates the list item and the cross to remove the list item*/
+        var input_value = $('input').val();
+        $('ul').append('<li>' + input_value + '<a href="" id="cross">x</a></li>');
+
+    };
+    /* If the input value is an empty string do nothing*/
+    $('input').val('');
+    return false;
+});
+/* Function to remove the list item */
+$(document).on('click', '#cross', function (e) {
+    /* Makes sure the href doesn't take the user anywhere */
+    e.preventDefault();
+    $(this).parent().remove();
+});
